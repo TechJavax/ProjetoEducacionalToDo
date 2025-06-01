527c249075c819515acfab9937739fde18366994
Emerson França <mars.javax@gmail.com>
2025-06-01 18:10:57 -0300

    First commit
    Configurações básicas para a aplicação, como estrutura de pastas, instalação e atualização de dependências e a criação da classe Model.

---
+++ API/src/main/java/TODO/API/model/TaskModel.java
@@ -0,0 +1,56 @@
+package TODO.API.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table()
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is mandatory")
+    @Size(min = 3, max = 100)
+    private String title;
+
+    @NotBlank(message = "Description is mandatory")
+    @Size(min = 100, max = 500)
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+    public TaskModel(){
+
+    }
+
+    public TaskModel(Long id, String title, String description, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
